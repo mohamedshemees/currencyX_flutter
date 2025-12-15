940dccf3fa8a597c4abae259633a47921c99cf62
mohamedshemees <mohamedessampd@gmail.com>
2025-12-16 01:06:15 +0200

    create country class with init data

--- lib/main.dart
+++ lib/main.dart
@@ -25,8 +25,79 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: <Widget>[
-     // BaseCurrencyWidget()
-    ]);
+    return Scaffold(
+      body: SafeArea(
+        child: Column(crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+          Text("Currency"),
+          Text("Exchange"),
+          BaseCurrencyWidget()
+        ],
+        ),
+      )
+    );
   }
 }
+class BaseCurrencyWidget extends StatelessWidget {
+  const BaseCurrencyWidget({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        child: Column(
+          children: <Widget>[
+            Row(
+            children: [
+              Text("From"),
+              CountriesDropdownWidget()
+            ],
+            ),
+            Text("Amro Currency"),
+          ],
+        ) ,
+      ),
+    );
+  }
+}
+class CountriesDropdownWidget extends StatelessWidget {
+  const CountriesDropdownWidget({super.key});
+  @override
+  Widget build(BuildContext context) {
+  return DropdownButton<Country>(
+    items: countries.map((Country country) {
+      return DropdownMenuItem<Country>(
+        value: country,
+        child: Row(
+          
+          children: <Widget>[
+            Text(country.flag),
+             Column(
+          children: [
+            Text(country.abbreviation),
+            Text(country.name),
+          ],
+        )])
+      );
+    }).toList(),
+    onChanged: (value) {
+      print(value);
+    },
+
+  );
+
+  }
+}
+
+class Country{
+  String abbreviation;
+  String flag;
+  String name;
+  Country({required this.abbreviation, required this.flag, required this.name});
+}
+var countries = [
+  Country(abbreviation: "USD", flag: "🇺🇸", name: "United States"),
+  Country(abbreviation: "CAD", flag: "🇨🇦", name: "Canada"),
+  Country(abbreviation: "INR", flag: "🇮🇳", name: "India"),
+  Country(abbreviation: "EUR", flag: "🇪🇺", name: "Europe"),
+];
