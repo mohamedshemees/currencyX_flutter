292b724fe293068dfa3aedaa691c7dba10f4b5b6
mohamedshemees <mohamedessampd@gmail.com>
2025-12-16 07:00:40 +0200

    feat: add app colors

---
+++ lib/app_colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+
+  AppColors._();
+
+  static const Color gradientStart = Color(0xFFF8F9FA);
+  static const Color gradientEnd = Color(0xFF684135);
+  static const Color backgroundColor = Color(0xFFFBFDFF);
+  static const Color cardBackgroundColor = Color(0xFFFFFFFF);
+  static const Color spinnerBorderColor = Color(0xFFE2E8F0);
+  static const Color textColor = Color(0xFF1A202C);
+  static const Color subtleTextColor = Color(0xFF7A828A);
+  static const Color iconColor = Color(0xFF1A202C);
+  static const Color accentColor = Color(0xFFF5A623);
+  static const Color onAccentColor = Color(0xFFFFFFFF);
+}
