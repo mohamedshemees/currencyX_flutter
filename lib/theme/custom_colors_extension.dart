2968e99d88fa5e045a666b0f581a7a46ae5f83fa
mohamedshemees <mohamedessampd@gmail.com>
2026-01-01 22:48:30 +0200

    feat: add theme cubit and icon color

--- lib/theme/custom_colors_extension.dart
+++ lib/theme/custom_colors_extension.dart
@@ -1,50 +1,79 @@
 import 'package:currencyx/theme/app_colors.dart';
 import 'package:flutter/material.dart';
-
-class CustomColorsExtension {
+@immutable
+class CustomColorsExtension extends ThemeExtension<CustomColorsExtension> {
   final Color textColor;
+  final Color subTextColor;
   final Color iconColor;
   final Color gradientStart;
   final Color gradientEnd;
   final Color cardBackgroundColor;
+  final String themeIcon;
 
-
-  CustomColorsExtension._({
+  const CustomColorsExtension({
     required this.textColor,
+    required this.subTextColor,
     required this.iconColor,
     required this.gradientStart,
     required this.gradientEnd,
     required this.cardBackgroundColor,
+    required this.themeIcon,
   });
 
-  factory CustomColorsExtension._light() {
-    return CustomColorsExtension._(
-      textColor: AppColors.textColor,
-      iconColor: AppColors.iconColor,
-      gradientStart: AppColors.gradientStart,
-      gradientEnd: AppColors.gradientEnd,
-      cardBackgroundColor: AppColors.cardBackgroundColor,
+  static const light = CustomColorsExtension(
+    textColor: AppColors.textColor,
+    subTextColor: AppColors.subtleTextColor,
+    iconColor: AppColors.iconColor,
+    gradientStart: AppColors.gradientStart,
+    gradientEnd: AppColors.gradientEnd,
+    cardBackgroundColor: AppColors.cardBackgroundColor,
+    themeIcon: 'assets/ic_sun.svg',
+  );
+
+  static const dark = CustomColorsExtension(
+    textColor: AppColors.darkTextColor,
+    subTextColor: AppColors.darkSubtleTextColor,
+    iconColor: AppColors.darkIconColor,
+    gradientStart: AppColors.darkGradientStart,
+    gradientEnd: AppColors.darkGradientEnd,
+    cardBackgroundColor: AppColors.darkCardBackgroundColor,
+    themeIcon: 'assets/ic_moon.svg',
+  );
+
+  @override
+  CustomColorsExtension copyWith({
+    Color? textColor,
+    Color? iconColor,
+    Color? gradientStart,
+    Color? gradientEnd,
+    Color? cardBackgroundColor,
+  }) {
+    return CustomColorsExtension(
+      textColor: textColor ?? this.textColor,
+      subTextColor: subTextColor,
+      iconColor: iconColor ?? this.iconColor,
+      gradientStart: gradientStart ?? this.gradientStart,
+      gradientEnd: gradientEnd ?? this.gradientEnd,
+      cardBackgroundColor: cardBackgroundColor ?? this.cardBackgroundColor,
+      themeIcon: themeIcon,
     );
   }
 
-  factory CustomColorsExtension._dark() {
-    return CustomColorsExtension._(
-        textColor: AppColors.darkTextColor,
-        iconColor: AppColors.darkIconColor,
-        gradientStart: AppColors.darkGradientStart,
-        gradientEnd: AppColors.darkGradientEnd,
-        cardBackgroundColor: AppColors.darkCardBackgroundColor,
+  @override
+  CustomColorsExtension lerp(
+      covariant ThemeExtension<CustomColorsExtension>? other,
+      double t,
+      ) {
+    if (other is! CustomColorsExtension) return this;
 
+    return CustomColorsExtension(
+      textColor: Color.lerp(textColor, other.textColor, t)!,
+      subTextColor: Color.lerp(subTextColor, other.subTextColor, t)!,
+      iconColor: Color.lerp(iconColor, other.iconColor, t)!,
+      gradientStart: Color.lerp(gradientStart, other.gradientStart, t)!,
+      gradientEnd: Color.lerp(gradientEnd, other.gradientEnd, t)!,
+      cardBackgroundColor: Color.lerp(cardBackgroundColor, other.cardBackgroundColor, t)!,
+      themeIcon: themeIcon,
     );
-  }
-}
-
-extension CustomColors on ThemeData {
-  CustomColorsExtension get customColors {
-    if (brightness == Brightness.light) {
-      return CustomColorsExtension._light();
-    } else {
-      return CustomColorsExtension._dark();
-    }
   }
 }
--- lib/theme/theme_data_dark.dart
+++ lib/theme/theme_data_dark.dart
@@ -1,30 +1,9 @@
-
-import 'package:currencyx/theme/app_colors.dart';
-import 'package:currencyx/theme/app_fonts.dart';
+import 'package:currencyx/theme/custom_colors_extension.dart';
 import 'package:flutter/material.dart';
 
 ThemeData getDarkTheme() {
   return ThemeData(
-    primarySwatch: Colors.blue,
-    fontFamily: AppFonts.roboto,
-    primaryColor: AppColors.onAccentColor,
-    colorScheme: ColorScheme.fromSeed(
-      seedColor: AppColors.accentColor,
-      brightness: Brightness.dark,
-    ),
-
+    extensions: [CustomColorsExtension.dark],
     brightness: Brightness.dark,
-    appBarTheme: const AppBarTheme(
-      backgroundColor: Colors.black,
-      elevation: 0,
-      iconTheme: IconThemeData(
-        color: Colors.white,
-      ),
-      titleTextStyle: TextStyle(
-        color: Colors.white,
-        fontSize: 20,
-        fontWeight: FontWeight.bold,
-      ),
-    ),
   );
 }
